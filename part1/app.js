20526bfbf6992bcb402d173764ce67be2972caf6
yousef badr <yousefbadr10@gmail.com>
2024-02-06 00:02:42 +0200

    edit part1

--- part1/app.js
+++ part1/app.js
@@ -1,26 +1,25 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
 const srv = http.createServer((req, res) => {
-  if (req.url.startsWith('/public/')) {
-    const filePath = path.join(__dirname, req.url);
-    fs.readFile(filePath, (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+  if (req.url == '/') {
+    res.writeHead(200,{ 'Content-Type': 'text/html' });
+    fs.readFile("views/home.html",function(err,data){
       res.write(data);
-      return res.end();
-    });
-  }
-
-  else if (req.url == '/') {
-    const filePath = path.join(__dirname, '/views/home.html');
-    return servePage(filePath, res);
+      res.end();
+    })
   } else if (req.url == '/about') {
-    const filePath = path.join(__dirname, '/views/about.html');
-    return servePage(filePath, res);
+    res.writeHead(200,{ 'Content-Type': 'text/html' });
+    fs.readFile("views/about.html",function(err,data){
+      res.write(data);
+      res.end();
+    })
   } else if (req.url == '/contact') {
-    const filePath = path.join(__dirname, '/views/contact.html');
-    return servePage(filePath, res);
+    res.writeHead(200,{ 'Content-Type': 'text/html' });
+    fs.readFile("views/contact.html",function(err,data){
+      res.write(data);
+      res.end();
+    })
   }
 
   else {
@@ -28,18 +27,6 @@ const srv = http.createServer((req, res) => {
     res.end('404 Not Found');
   }
 });
-
-function servePage(filePath, res) {
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      return res.end('404 Not Found');
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
-  });
-}
 
 srv.listen(8080);
 
